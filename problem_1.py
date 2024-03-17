a15f849f981c319868de95a729fbb4d7cf0b97f9
Andrei Lebedev <andjey_777@mail.ru>
2024-03-17 12:39:48 +0300

    auto prob 1 test

--- problem_1.py
+++ problem_1.py
@@ -1,16 +1,26 @@
-#1. Two Sum
-#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-#You may assume that each input would have exactly one solution, and you may not use the same element twice.
-#You can return the answer in any order.
-print(123)
-import time
-time.sleep(2)
-nums = [1, 2, 3, 4, 8, 6]
-target = 10
-for i in range(len(nums)):
-    for j in range(len(nums)):
-        if i == j:
-            continue
-        if nums[i] + nums[j] == target:
-            print(i, j)
-            exit()
+# 1. Two Sum
+# https://leetcode.com/problems/two-sum/description/
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+
+vars = [
+    ([2,7,11,15], 9, [0,1]),
+    ([3,2,4], 6, [1,2]),
+    ([3,3], 6, [0,1])
+    ]
+nums = [2,7,11,15]
+target = 9
+def Two_Sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i == j:
+                continue
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+for i in vars:
+    if Two_Sum(i[0], i[1]) == i[2]:
+        print(i, "Success")
+    else:        
+        raise Exception("Wrong result at", i)
