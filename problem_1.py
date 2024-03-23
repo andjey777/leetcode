59ac56afb1be9c85e74a91f8c0425676fb813e07
Andrei Lebedev <andjey_777@mail.ru>
2024-03-23 12:46:28 +0300

    test dict instaad of list

--- problem_1.py
+++ problem_1.py
@@ -5,13 +5,17 @@
 # https://leetcode.com/problems/two-sum/description/
 
 
-vars = [
-    ([2, 7, 11, 15], 9, [0, 1]),
-    ([3, 2, 4], 6, [1, 2]),
-    ([3, 3], 6, [0, 1]),
-    ([1, 5, 8, 12, 15, 16], 20, [1, 4]),
-    ([2, 5, 7, 8, 14, 15, 22, 45], 59, [4, 7]),
-]
+var = {
+    "input": [
+        [2, 7, 11, 15],
+        [3, 2, 4],
+        [3, 3],
+        [1, 5, 8, 12, 15, 16],
+        [2, 5, 7, 8, 14, 15, 22, 45],
+    ],
+    "target": [9, 6, 6, 20, 59],
+    "result": [[0, 1], [1, 2], [0, 1], [1, 4], [4, 7]],
+}
 
 
 def Two_Sum(nums, target):
@@ -23,6 +27,6 @@ def Two_Sum(nums, target):
                 return [i, j]
 
 
-for i in vars:
-    if Two_Sum(i[0], i[1]) != i[2]:
-        raise Exception("Wrong result at", i)
+for i in range(len(var["input"])):
+    if Two_Sum(var["input"][i], var["target"][i]) != var["result"][i]:
+        raise Exception("Wrong result at dataset #", i)
