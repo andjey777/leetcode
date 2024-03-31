9d91e7da969f7c80199b72f79408d19209ad12b9
Andrei Lebedev <andjey_777@mail.ru>
2024-03-31 12:08:35 +0300

    two sum new test

---
+++ two_sum/two_sum.py
@@ -0,0 +1,14 @@
+# 1. Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+# https://leetcode.com/problems/two-sum/description/
+
+
+def Two_Sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i == j:
+                continue
+            if nums[i] + nums[j] == target:
+                return [i, j]
